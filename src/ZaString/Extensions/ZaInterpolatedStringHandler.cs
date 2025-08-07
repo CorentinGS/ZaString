e7d0cb0ed2d55b03ec45bf913aac4a057ca99492
Corentin Giaufer Saubert <43623834+CorentinGS@users.noreply.github.com>
2025-08-07 22:36:04 +0200

    feat: Implement ZaInterpolatedStringHandler for enhanced string interpolation support in ZaSpanStringBuilder, including new extension methods for appending interpolated strings

---
+++ src/ZaString/Extensions/ZaInterpolatedStringHandler.cs
@@ -0,0 +1,57 @@
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using ZaString.Core;
+
+namespace ZaString.Extensions;
+
+[InterpolatedStringHandler]
+public ref struct ZaInterpolatedStringHandler
+{
+    private ZaSpanStringBuilder _builder;
+    private readonly IFormatProvider? _provider;
+
+    public ZaInterpolatedStringHandler(int literalLength, int formattedCount, ref ZaSpanStringBuilder builder)
+    {
+        _builder = builder;
+        _provider = CultureInfo.InvariantCulture;
+    }
+
+    public ZaInterpolatedStringHandler(int literalLength, int formattedCount, ref ZaSpanStringBuilder builder, IFormatProvider? provider)
+    {
+        _builder = builder;
+        _provider = provider ?? CultureInfo.InvariantCulture;
+    }
+
+    public void AppendLiteral(string value)
+    {
+        _builder.Append(value);
+    }
+
+    public void AppendFormatted(string? value)
+    {
+        _builder.Append(value);
+    }
+
+    public void AppendFormatted(ReadOnlySpan<char> value)
+    {
+        _builder.Append(value);
+    }
+
+    public void AppendFormatted(char value)
+    {
+        _builder.Append(value);
+    }
+
+    public void AppendFormatted<T>(T value) where T : ISpanFormattable
+    {
+        _builder.Append(value, default, _provider);
+    }
+
+    public void AppendFormatted<T>(T value, string? format) where T : ISpanFormattable
+    {
+        _builder.Append(value, format, _provider);
+    }
+
+    public ZaSpanStringBuilder GetResult() => _builder;
+}
+
